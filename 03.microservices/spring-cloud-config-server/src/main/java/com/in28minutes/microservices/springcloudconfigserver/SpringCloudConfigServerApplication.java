c8d1eb42ec29d3e10b94e4b3adf045cdb9e56c6e
lakhminder <lakhminder@gmail.com>
2018-04-29 16:39:41 +0530

    added comments

--- 03.microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ 03.microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -4,6 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Steps to create => git-localconfig-repo(git repo created locally)
+ * > cd git-localconfig-repo
+ * > git init
+ * --create properties file(limits-service.properties)
+ * > git add -A
+ * > git commit -m "message"
+ * 
+ * then change the application.properties to point to our local git repo
+ * 
+ * refer pom.xml for changes
+ * 
+ * check http://localhost:8888/limits-service/default => for properties
+ *		 http://localhost:8888/limits-service/{ENV} => for dev/qa
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
