4cf91f463b2bd38902aeddbd8f438f73094d4b57
lakhminder <lakhminder@gmail.com>
2018-04-29 16:39:02 +0530

    added comments

--- 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Configuration.java
+++ 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Configuration.java
@@ -3,6 +3,18 @@ package com.in28minutes.microservices.limitsservice;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * @ConfigurationProperties look for properties in application.properties as
+ * limits-service.minimum=99
+ *
+ * however here we are pulling from GIT repo so 
+ * 1) properties are not defined here(application.properties)
+ * 2) use bootstrap.properties instead of application.properties
+ * 3) define spring.cloud.config.uri to connect to
+ * check spring-cloud-config-server
+ * 
+ *
+ */
 @Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
