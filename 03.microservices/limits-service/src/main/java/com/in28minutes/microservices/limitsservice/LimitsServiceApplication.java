ca7f62d73707f40c7b27bfb800702bcf2aa49dd1
lakhminder <lakhminder@gmail.com>
2018-05-07 15:29:33 +0530

    comments for hysterix

--- 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**
  * 
  * check Configuration.java
+ * 
+ * Hystrix is used for Fault tolerance
+ * 	1) define dependency in pom
+ * 	2) @EnableHystrix in application
+ * 	3) use @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration") to define fallback method
+ * 		which will be invoked incase of any Exception	 
+ * 		refer Controller
  *
  */
 @SpringBootApplication
