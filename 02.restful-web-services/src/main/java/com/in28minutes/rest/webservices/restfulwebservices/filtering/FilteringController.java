bf2b4ecfdc595841c5320397ba98224c4b00062b
lakhminder <lakhminder@gmail.com>
2018-05-20 17:05:39 +0530

    filtering fields from Response bean

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -7,14 +7,31 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
+/**
+ * Static Filtering
+ * 		Add @JsonIgnore or @JsonIgnoreProperties
+ * 		refer SomeBean.java
+ * Dynamic Filtering(diff methods can return diff fields)
+ * 		MappingJacksonValue of bean is returned
+ * 		Add @JsonFilter with same filter name as in FilterProvider
+ * 		refer retrieveSomeBean() and SomeBean.java
+ * 
+ *
+ */
 @RestController
 public class FilteringController {
 
-	// field1,field2
+	/*@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+		return new SomeBean("value1", "value2", "value3");
+	}*/
+	//Version 2 Dynamic Filtering
+	// field1,field2 will be returned
 	@GetMapping("/filtering")
 	public MappingJacksonValue retrieveSomeBean() {
 		SomeBean someBean = new SomeBean("value1", "value2", "value3");
@@ -24,13 +41,13 @@ public class FilteringController {
 		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
 
 		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
-
+		
 		mapping.setFilters(filters);
 
 		return mapping;
 	}
 
-	// field2, field3
+	// field2, field3 will be returned
 	@GetMapping("/filtering-list")
 	public MappingJacksonValue retrieveListOfSomeBeans() {
 		List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
